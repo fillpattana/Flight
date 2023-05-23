a622bdf7b6cae2fdd5591a891a7bacfa2ac132cf
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2023-05-24 00:37:31 +0700

    Card constructors

--- Payment/card.py
+++ Payment/card.py
@@ -1,19 +1,27 @@
-from abc import ABC, abstractmethod
+class Card():
+    
+    def __init__(self, fname, lname, balance, id, pin):
+        self.fname = fname
+        self.lname = lname
+        self.balance = balance
+        self.pin = pin
+        self.id = 1234
+        self.cards = []
 
-class Card(ABC):
-    def __init__(self, name, balance, pin):
-            self.name = name
-            self.balance = balance
-            self.pin = pin
-            
-    @abstractmethod
-    def getBalance(self):
-        return self.balance
-    
-    def setBalance(self, b):
-        self.balance += b
-        
-    def addCard(self):
-        pass
-        
-    
+    def addCard(self, card):
+        self.cards.append(card)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    obj1 = Card("ja", "jo", 1, 000, 2)
+    print(obj1.fname)
--- Payment/creditCard.py
+++ Payment/creditCard.py
@@ -1,10 +1,36 @@
-from Payment import card
+from card import Card
 
-class creditCard(card):
-    def __init__(self, name, balance, pin):
-        self.name = name
-        self.balance = balance
+class creditCard(Card):
+    
+    def __init__(self, fname, lname, id, pin):
+        self.fname = fname
+        self.lname = lname
+        self.id = id
         self.pin = pin
-        
-    def getBalance(self):
-        return self.balance
+        self.limit = None
+    
+    def confirmPay():
+        pass
+    
+    def setLimit(self, limit):
+        self.limit = limit
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    obj2 = creditCard("ji", "ju", 9902, 3)
+    print(obj2.fname, obj2.lname, obj2.pin)
