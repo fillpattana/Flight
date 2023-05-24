4bd6623a3ec09e375b8c071a92101ba986371f27
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2023-05-25 00:34:53 +0700

    user constructor

---
+++ User/user.py
@@ -0,0 +1,9 @@
+class User():
+    
+    def __init__(self, name, age, usern, pw):
+        self.name = name
+        self.age = age
+        self.usern = usern
+        self.pw = pw
+        
+    
